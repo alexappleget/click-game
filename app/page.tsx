ab0a3fbc0f683cd5dc18cfef9d66e54254f85277
Alex Appleget <alexappleget2014@gmail.com>
2024-11-13 12:06:18 -0600

    Fix: Deleted border

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LinkCustom } from "@/components/LinkCustom/LinkCustom";
 
 export default function Home() {
   return (
-    <section className="border-2 border-black h-screen flex flex-col items-center justify-center gap-14">
+    <section className="h-screen flex flex-col items-center justify-center gap-14">
       <p>
         This is a click-based game. All you will need is a mouse to click
         around!
